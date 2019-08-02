0d4528ccb22e0e574bec4d754c7393912419cd10
deepan <deepan.mathi92@gmail.com>
2019-08-02 00:52:55 -0400

    change name of spring data main java

--- Spring-boot-Spring-data-api/src/main/java/com/springboot/springData/SpringBootSpringDataApiApplication.java
+++ Spring-boot-Spring-data-api/src/main/java/com/springboot/springData/SpringBootSpringDataApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootJpaNativeApiApplication {
+public class SpringBootSpringDataApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJpaNativeApiApplication.class, args);
+		SpringApplication.run(SpringBootSpringDataApiApplication.class, args);
 	}
 
 }
